98af345864042c54d44d8af4f1233814c4651f44
Stefanus Dwi Wijanarko <stefanus.dwi.1904226@students.um.ac.id>
2022-10-27 07:24:36 +0700

    php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "hello world!<br>";
+
+$nama = "Stefanus";
+$umur = 20;
+
+echo "Nama saya $nama, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Dwi";
+$umurAdik = 15;
+
+echo "Nama adik saya $namaAdik, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+
+?>
